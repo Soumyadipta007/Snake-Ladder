75638a3ef5908eacc68b56a2bc7a839f8be1ec99
Soumya <soumyadipta0077@gmail.com>
2020-09-30 14:08:27 +0530

    [Soumya] Refactor: Rolling till Player wins

--- Snake_Ladder/Snake_Ladder/Program.cs
+++ Snake_Ladder/Snake_Ladder/Program.cs
@@ -1,23 +1,26 @@
 ﻿using System;
 
+namespace Snake_Ladder
 {
     class Program
     {
         static void Main(string[] args)
         {
             int position = 0;
-            Console.WriteLine("Position: "+position);
+            Console.WriteLine("Position: " + position);
+            const int FinalPosition = 100;
             const int NOPLAY = 1;
             const int LADDER = 2;
-	    const int SNAKE = 3;
-            while (position < 100)
+            const int SNAKE = 3;
+            while (position < FinalPosition)
+            {
                 Random random = new Random();
                 int dieNumber = random.Next(1, 7);
                 Console.WriteLine("Number came from die: " + dieNumber);
-                if(position+dieNumber>=FinalPosition)
+                if (position + dieNumber >= FinalPosition)
                 {
-                    int newPosition=position+dieNumber;
-                    Console.WriteLine("New Position: "+newPosition);
+                    int newPosition = position + dieNumber;
+                    Console.WriteLine("New Position: " + newPosition);
                     break;
                 }
                 int options = random.Next(1, 4);
