d91f6f55304b75360829e6e1818884ff0e321f2d
Soumya <soumyadipta0077@gmail.com>
2020-09-30 11:37:30 +0530

    [Soumya] Add:Adding Starting Position

---
+++ Snake_Ladder/Snake_Ladder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Snake_Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            Console.WriteLine("Position: "+position);
+        }
+    }
+}
