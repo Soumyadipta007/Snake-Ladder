8c5fb082b6cfa9cd5748cc610640acd2a49bd7fb
Soumya <soumyadipta0077@gmail.com>
2020-09-30 11:58:31 +0530

    [Soumya] Refactor:Checking for options

--- Snake_Ladder/Snake_Ladder/Program.cs
+++ Snake_Ladder/Snake_Ladder/Program.cs
@@ -11,6 +11,28 @@ namespace Snake_Ladder
             Random random = new Random();
             int dieNumber = random.Next(1, 7);
             Console.WriteLine("Number came from die: " + dieNumber);
+            int options = random.Next(1, 4);
+            const int NOPLAY = 1;
+            const int LADDER = 2;
+            const int SNAKE = 3;
+            switch (options)
+            {
+                case NOPLAY:
+                    Console.WriteLine("No Play");
+                    break;
+                case LADDER:
+                    Console.WriteLine("Ladder");
+                    position += dieNumber;
+                    break;
+                case SNAKE:
+                    Console.WriteLine("Snake");
+                    if (position - dieNumber > 0)
+                        position -= dieNumber;
+                    else
+                        position = 0;
+                    break;
+            }
+            Console.WriteLine("New Position: " + position);
         }
     }
 }
