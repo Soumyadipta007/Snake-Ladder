28e85613c0d585e5259a5cb8ccb52cade0b4e6d7
Soumya <soumyadipta0077@gmail.com>
2020-09-30 12:05:51 +0530

    [Soumya] Refactor: Rolling Till Player Wins

--- Snake_Ladder/Snake_Ladder/Program.cs
+++ Snake_Ladder/Snake_Ladder/Program.cs
@@ -8,31 +8,34 @@ namespace Snake_Ladder
         {
             int position = 0;
             Console.WriteLine("Position: "+position);
-            Random random = new Random();
-            int dieNumber = random.Next(1, 7);
-            Console.WriteLine("Number came from die: " + dieNumber);
-            int options = random.Next(1, 4);
-            const int NOPLAY = 1;
-            const int LADDER = 2;
-            const int SNAKE = 3;
-            switch (options)
+            while (position < 100)
             {
-                case NOPLAY:
-                    Console.WriteLine("No Play");
-                    break;
-                case LADDER:
-                    Console.WriteLine("Ladder");
-                    position += dieNumber;
-                    break;
-                case SNAKE:
-                    Console.WriteLine("Snake");
-                    if (position - dieNumber > 0)
-                        position -= dieNumber;
-                    else
-                        position = 0;
-                    break;
+                Random random = new Random();
+                int dieNumber = random.Next(1, 7);
+                Console.WriteLine("Number came from die: " + dieNumber);
+                int options = random.Next(1, 4);
+                const int NOPLAY = 1;
+                const int LADDER = 2;
+                const int SNAKE = 3;
+                switch (options)
+                {
+                    case NOPLAY:
+                        Console.WriteLine("No Play");
+                        break;
+                    case LADDER:
+                        Console.WriteLine("Ladder");
+                        position += dieNumber;
+                        break;
+                    case SNAKE:
+                        Console.WriteLine("Snake");
+                        if (position - dieNumber > 0)
+                            position -= dieNumber;
+                        else
+                            position = 0;
+                        break;
+                }
+                Console.WriteLine("New Position: " + position);
             }
-            Console.WriteLine("New Position: " + position);
         }
     }
 }
